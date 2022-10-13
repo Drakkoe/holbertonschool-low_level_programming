3be5b4b790702759c0f91047f93d0085bc9d51f8
Harold Garzon <5038@holbertonstudents.com>
2022-10-13 08:30:49 -0700

    correcting 5 file

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- *File: 5-printf.c
- *Authr: Harold Garzon
- *ID: <5838@holbertonstudents.com
+ * File: 5-printf.c
+ * Authr: Harold Garzon
+ * ID: <5838@holbertonstudents.com
  *
- *main - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("With propper grammar, but the outcome is a piece of art,\n");
+	printf("With proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
