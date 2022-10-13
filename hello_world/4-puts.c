3adf469bc0eef798075022f191d711845843e5d2
Harold Garzon <5038@holbertonstudents.com>
2022-10-12 20:51:46 -0700

    adding new file 4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+ *File: 4-puts.c
+ *Author: Harold Garzon <5838@holbertonstudents.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like bulding a multilingual puzzle");
+
+	returno (0);
+}
