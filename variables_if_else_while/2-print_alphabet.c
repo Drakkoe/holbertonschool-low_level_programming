23331ddd1cf4402c187786999594c50d03747588
Harold Garzon <5038@holbertonstudents.com>
2022-10-14 22:28:12 -0700

    adding new 2 file

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* The alphabet in lowe case */
+/**
+ * main - Standart input
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
