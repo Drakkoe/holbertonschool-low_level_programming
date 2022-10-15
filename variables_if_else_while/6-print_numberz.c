5c666fb58736066e6fdeedf4fa929e6314160da1
Harold Garzon <5038@holbertonstudents.com>
2022-10-14 23:22:56 -0700

    adding new file 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* Prints single digit number of base 10 starting from 0 */
+/**
+ * main - standard input
+ *
+ * Return : Always 0
+ */
+int main(void)
+{
+	int numberz;
+
+	for (numberz = 0; numberz < 10; numberz++)
+			putchar((numberz % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
