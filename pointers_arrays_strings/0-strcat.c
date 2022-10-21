b339716711741c6bb4dd7080fd9cdf1cdba2cadd
Harold Garzon <5038@holbertonstudents.com>
2022-10-21 13:08:24 -0700

    updating

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,11 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int count1; int count2;
+	int count1 = 0;
+	int count2 = 0;
 
-	count1=0;
-	while(dest[count1] != '\0')
-	{
-		count1++;
-	}
-	for(count2 = 0; src[count2] != '\0'; count2++, count1++)
+	for (count1 = 0; dest[count1] != '\0'; count1++)
+	for (count2 = 0; src[count2] != '\0'; count2++, count1++)
 	{
 		dest[count1] = src[count2];
 	}
