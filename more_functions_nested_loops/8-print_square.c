becd61757434e2e5a2e5e2829fbb7e4e4ff4ce53
Harold Garzon <5038@holbertonstudents.com>
2022-10-21 20:47:38 -0700

    adding new 8 file

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_square - check the code
+ *
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
