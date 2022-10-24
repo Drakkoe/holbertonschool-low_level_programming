159ea8bd902dd1b1cb25c988803bba921f6f693a
Harold Garzon <5038@holbertonstudents.com>
2022-10-24 13:24:07 -0700

    adding new file

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,4 @@
-#include "main.h"
+u#include "main.h"
 #include <stdio.h>
 /**
  * _strlen - counts the strign
--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,7 @@
 0;10;1c#include "main.h"
 #include <stdio.h>
 #include <string.h>
+#Include <2-strlen.c>
 /**
  * rev_string - reverses a string
  *
@@ -8,18 +9,11 @@
  */
 void rev_string(char *s);
 {
-	int a = 0;
-	int b = 0;
-	char c;
+	int i = strlen(s) -1;
 
-	while (s[b++])
+	while (i != -1)
 	{
-		a++;
+		_putchar("%c"s[i--]);
 	}
-	for (b = a -1; b >= a / 2 ; b--)
-	{
-		c = s[b];
-		s[b] = s[a - b - 1];
-		s[a - b - 1] = c;
-	}
+	_putchar('\n');
 }
