f152a14e95aec0d2b1c25988f93651a019f90d23
Harold Garzon <5038@holbertonstudents.com>
2022-10-24 13:40:53 -0700

    adding new 8 file

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements in the array
+ * @a: array to be printed
+ * @n: number of elements to be printed
+ *
+ * Return: always 0
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i < (n - 1))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
