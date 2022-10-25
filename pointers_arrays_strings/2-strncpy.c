5ee9e6999a2aa29f60a4c737442890d9a0acaf05
Harold Garzon <5038@holbertonstudents.com>
2022-10-25 14:52:51 -0700

    adding new file

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
- *
  * _strncpy - copies n bytes of src to dest string
  * @dest: string 1
  * @src: string 2
  * @n: largest number of bytes
+ *
+ * Return: Dest Address
  */
 char *_strncpy(char *dest, char *src, int n)
 {
