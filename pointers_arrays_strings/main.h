80d612187a223350fa52d62cd542b4ea89792914
Harold Garzon <5038@holbertonstudents.com>
2022-10-25 15:17:00 -0700

    adding new 5 file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 void _puts(char *str);
+char *string_toupper(char *);
 int _putchar(char c);
 void print_rev(char *s);
 char *_strcat(char *dest, char *src);
